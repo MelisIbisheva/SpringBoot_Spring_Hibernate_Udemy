eec90d746e735d11ea964ef928a1a55d17220706
Melis Ibisheva <93177517+MelisIbisheva@users.noreply.github.com>
2025-02-25 22:47:27 +0200

    REST CRUD APIs

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-employee -with-spring-data-jpa/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee , Integer> {
+
+    //that's it...don't need to ride any cod LOL
+}
